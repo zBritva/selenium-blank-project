de665539cbd19e22a07ac875c156c8bb83a30ee7
Ilfat Galiev <ilfat.galiev@akvelon.com>
2018-01-16 20:06:24 +0300

    Update test

--- test/specs/powerapps/jasmine.spec.js
+++ test/specs/powerapps/jasmine.spec.js
@@ -3,7 +3,7 @@ describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function() {
         browser.url('/');
         var title = browser.getTitle();
-        expect(title).toBe('IIS Windows');
+        expect(title).toBe('This page can’t be displayed');
     });
 
 });
