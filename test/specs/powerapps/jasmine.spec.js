50b0c4114ee5ff4d951b89691e503c63590aec7c
Ilfat Galiev <ilfat.galiev@akvelon.com>
2018-01-16 20:13:56 +0300

    Read env vals

--- test/specs/powerapps/jasmine.spec.js
+++ test/specs/powerapps/jasmine.spec.js
@@ -1,8 +1,20 @@
 describe('webdriver.io page', function() {
 
+    let dxtLogin = process.env.dxtLogin;
+    let dxtPassword = process.env.dxtLogin;
+
+    let msitLogin = process.env.dxtLogin;
+    let msitPassword = process.env.dxtLogin;
+
+    let prodLogin = process.env.dxtLogin;
+    let prodPassword = process.env.dxtLogin;
+
     it('should have the right title - the fancy generator way', function() {
         browser.url('/');
         var title = browser.getTitle();
+        console.log(dxtLogin);
+        console.log(msitLogin);
+        console.log(prodLogin);
         expect(title).toBe('This page can’t be displayed');
     });
 
